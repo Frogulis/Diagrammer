551e317e38611f516370a038d7734c81ab22e565
Jamie Hoffmann <jamie.hoffmann@xero.com>
2019-08-06 16:43:34 +1000

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+screen.fill((255, 0, 0))
+
+pygame.draw.circle(screen, (0,0,255), (150, 100), 100)
+
+pygame.image.save(screen, "C:\\Dev\\Diagrammer\\output\\test.png")
