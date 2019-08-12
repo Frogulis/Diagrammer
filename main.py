a73a94807f0bab0fa273a33a42efcb0e07961d63
Jamie Hoffmann <jamsarhoff@hotmail.com>
2019-08-12 22:41:21 +1000

    basic json load

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
+import json
 
-pygame.init()
+class Diagram:
+    def __init__(self):
+        self._diagram = None
+    
+    def loadFromJson(self, filename):
+        with open(filename, 'r') as f:
+            self._diagram = json.loads(f.read())
+            f.close()
+        print(self._diagram)
+    
+    def draw(self):
+        pygame.init()
+        screen = pygame.display.set_mode((640, 480))
+        screen.fill((255, 0, 0))
+        pygame.draw.circle(screen, (0,0,255), (150, 100), 100)
+        pygame.image.save(screen, ".\\output\\test.png")
 
-screen = pygame.display.set_mode((640, 480))
-
-screen.fill((255, 0, 0))
-
-pygame.draw.circle(screen, (0,0,255), (150, 100), 100)
-
-pygame.image.save(screen, ".\\output\\test.png")
+d = Diagram()
+d.loadFromJson("example.json")
