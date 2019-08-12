28918550c5b32cb39c45426833681685bc8bc42f
Jamie Hoffmann <jamsarhoff@hotmail.com>
2019-08-12 23:08:30 +1000

    basic block drawing

--- main.py
+++ main.py
@@ -3,20 +3,44 @@ import json
 
 class Diagram:
     def __init__(self):
-        self._diagram = None
+        self.diagram = None
+        self.font = None
     
     def loadFromJson(self, filename):
         with open(filename, 'r') as f:
-            self._diagram = json.loads(f.read())
+            self.diagram = json.loads(f.read())
             f.close()
-        print(self._diagram)
     
     def draw(self):
         pygame.init()
-        screen = pygame.display.set_mode((640, 480))
-        screen.fill((255, 0, 0))
-        pygame.draw.circle(screen, (0,0,255), (150, 100), 100)
+        self.loadFonts()
+        screen = pygame.display.set_mode((1024, 768))
+        screen.fill((255, 255, 255))
+        self.drawDiagram(screen)
         pygame.image.save(screen, ".\\output\\test.png")
+        pygame.quit()
+    
+    def loadFonts(self):
+        pygame.font.init()
+        fname = pygame.font.get_default_font()
+        self.font = pygame.font.Font(fname, 12)
+    
+    def getTextSurface(self, text):
+        if not pygame.font.get_init():
+            raise Exception("intialise fonts first")
+        return self.font.render(text, 1, (0,0,0))
+
+    def drawDiagram(self, surface):
+        for block in self.diagram["blocks"]:
+            self.drawBlock(surface, block)
+    
+    def drawBlock(self, surface, block):
+        rect = pygame.Rect(block["x"], block["y"], 100, 50)
+        pygame.draw.rect(surface, (255,255,255), rect)
+        pygame.draw.rect(surface, (0,0,0), rect, 1)
+        textSurface = self.getTextSurface(block["text"])
+        surface.blit(textSurface, (rect.centerx - textSurface.get_width() / 2, rect.centery - textSurface.get_height() / 2))
 
 d = Diagram()
 d.loadFromJson("example.json")
+d.draw()
